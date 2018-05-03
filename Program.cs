5d2897ed87c74a9cf38eee17103e067aab57d53f
locvuwp2011 <locvuwp2011@live.com>
2018-05-03 22:35:09 +0700

    ticket-0000 initialize web app

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcore_web
+{
+    public class Program
+    {
+        // public static void Main(string[] args)
+        // {
+        //     BuildWebHost(args).Run();
+        // }
+
+        // public static IWebHost BuildWebHost(string[] args) =>
+        //     WebHost.CreateDefaultBuilder(args)
+        //         .UseStartup<Startup>()
+        //         .Build();
+
+        public static void Main(string[] args){
+            var configure = new ConfigurationBuilder().AddCommandLine(args).Build();
+
+            var host = new WebHostBuilder()
+                            .UseKestrel()
+                            .UseConfiguration(configure)
+                            .UseContentRoot(Directory.GetCurrentDirectory())
+                            .UseIISIntegration()
+                            .UseStartup<Startup>()
+                            .Build();
+
+            host.Run();
+        }
+    }
+}
